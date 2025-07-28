9c70b2ea53e32b0d5e383a567da178cbbd2be14a
jamesdebugsp <james.jhey020@icloud.com>
2025-07-28 12:58:38 -0300

    Adiciona exercício de operadores de atribuição em Java

---
+++ OperadoresAtribuicaoExercicio.java
@@ -0,0 +1,12 @@
+public class OperadoresAtribuicaoExercicio {
+    public static void main(String[] args) {
+        int numero = 5;        // Declara e inicializa a variável
+
+        numero += 3;           
+        numero -= 2;           
+        numero *= 4;          
+        numero /= 2;           
+
+        System.out.println("Valor final de numero: " + numero);
+    }
+}
